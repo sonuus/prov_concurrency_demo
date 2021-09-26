5608b3eb8ca249771b51f42371fc2b202f85d4b2
Sachin Doshi <sddoshi@amazon.com>
2021-09-26 19:40:58 -0400

    files added

--- lib/prov_concurrency-stack.ts
+++ lib/prov_concurrency-stack.ts
@@ -1,9 +1,21 @@
 import * as cdk from '@aws-cdk/core';
+import * as iam from '@aws-cdk/aws-iam'
+import * as dynamodb from '@aws-cdk/aws-dynamodb'
+import * as lambda from '@aws-cdk/aws-lambda'
+import * as sqs from '@aws-cdk/aws-sqs'
+import {SqsEventSource,SqsEventSourceProps} from '@aws-cdk/aws-lambda-event-sources'
+import * as apigateway from '@aws-cdk/aws-apigateway'
+import * as apigateway2 from '@aws-cdk/aws-apigatewayv2'
+import * as path from "path";
 
 export class ProvConcurrencyStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here test
+
+    new lambda.DockerImageFunction(this, "legacyFunction", {
+      code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, "../api/legacy_function")),
+    });
   }
 }
