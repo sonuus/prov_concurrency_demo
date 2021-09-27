9ff3f9c943c4159d5247e1aeef52394717fcc94b
Sachin Doshi <sddoshi@amazon.com>
2021-09-26 22:03:40 -0400

    files

--- lib/prov_concurrency-stack.ts
+++ lib/prov_concurrency-stack.ts
@@ -1,11 +1,14 @@
-import * as cdk from '@aws-cdk/core';
-import * as iam from '@aws-cdk/aws-iam'
-import * as dynamodb from '@aws-cdk/aws-dynamodb'
-import * as lambda from '@aws-cdk/aws-lambda'
-import * as sqs from '@aws-cdk/aws-sqs'
-import {SqsEventSource,SqsEventSourceProps} from '@aws-cdk/aws-lambda-event-sources'
-import * as apigateway from '@aws-cdk/aws-apigateway'
-import * as apigateway2 from '@aws-cdk/aws-apigatewayv2'
+import * as cdk from "@aws-cdk/core";
+import * as iam from "@aws-cdk/aws-iam";
+import * as dynamodb from "@aws-cdk/aws-dynamodb";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as sqs from "@aws-cdk/aws-sqs";
+import {
+  SqsEventSource,
+  SqsEventSourceProps,
+} from "@aws-cdk/aws-lambda-event-sources";
+import * as apigateway from "@aws-cdk/aws-apigateway";
+import * as apigateway2 from "@aws-cdk/aws-apigatewayv2";
 import * as path from "path";
 
 export class ProvConcurrencyStack extends cdk.Stack {
@@ -14,8 +17,40 @@ export class ProvConcurrencyStack extends cdk.Stack {
 
     // The code that defines your stack goes here test
 
-    new lambda.DockerImageFunction(this, "legacyFunction", {
-      code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, "../api/LegacyOrderPost/src/LegacyOrderPost")),
+    const legacyorderFunction = new lambda.DockerImageFunction(
+      this,
+      "legacyFunction",
+      {
+        code: lambda.DockerImageCode.fromImageAsset(
+          path.join(__dirname, "../api/LegacyOrderPost/src/LegacyOrderPost")
+        ),
+      }
+    );
+
+    const api = new apigateway.RestApi(this, "api", {
+      description: "Food orders api gateway",
+      deployOptions: {
+        stageName: "dev",
+      },
+      defaultCorsPreflightOptions: {
+        allowHeaders: [
+          "Content-Type",
+          "X-Amz-Date",
+          "Authorization",
+          "X-Api-Key",
+        ],
+        allowMethods: ["OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"],
+        allowCredentials: true,
+        allowOrigins: ["*"],
+      },
     });
+
+    const orderAPIResource = api.root.addResource("Orders");
+
+    orderAPIResource.addMethod(
+      "POST",
+      new apigateway.LambdaIntegration(legacyorderFunction, { proxy: true })
+    );
+    
   }
 }
